edf6f7daecf25f1b2e74a4f0c09cb8fe8fc53d4e
Chris McCormick <chrisjmccormick@gmail.com>
2018-11-06 16:22:03 -0800

    Shell script for running find_max_dataset multiple times

---
+++ find_max_dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python find_max_dataset.py
+python find_max_dataset.py
+python find_max_dataset.py
+python find_max_dataset.py
+python find_max_dataset.py
+python find_max_dataset.py
+python find_max_dataset.py
